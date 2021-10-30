19ae9e0ea61b1d5052bae4a593ed8296d0f4c1ff
hitg00 <hitgo17@gmail.com>
2021-10-31 04:45:10 +0530

    fix: homepage breaking
    Signed-off-by: hitg00 <hitgo17@gmail.com>

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,45 +1,69 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import GoogleLogin from 'react-google-login';
 import { useHistory, useLocation } from 'react-router-dom';
 import {
   useUserDispatch,
   loginUser,
 } from '../../components/context/UserContext';
-import Header from './Header'
+import Header from './Header';
 import HeroHome from './HeroHome';
 
+import AOS from 'aos';
+import { focusHandling } from 'cruip-js-toolkit';
 import '../../css/style.scss';
 
-const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
+const GOOGLE_CLIENT_ID =
+  process.env.REACT_APP_GOOGLE_CLIENT_ID ||
+  '86145416008-2ns2quak8ilnuk8vihpc17h4jv6l09dg.apps.googleusercontent.com';
 
-function LandingPage() {
-  let userDispatch = useUserDispatch();
-  let history = useHistory();
-  let { state } = useLocation();
+const AuthComponent = () => {
+  const userDispatch = useUserDispatch();
+  const history = useHistory();
+  const { state } = useLocation();
 
   return (
-    <>
-      <div>Welcome to Velomcity!!</div>
-      <GoogleLogin
-        clientId={GOOGLE_CLIENT_ID}
-        buttonText="Sign In with Google"
-        onSuccess={(response) => {
-          // setLoginLoading(true);
-          loginUser(userDispatch, history, response, state);
-        }}
-        onFailure={(err) => {
-          console.log(err);
-          // setLoginLoading(false);
-        }}
-      />
+    <GoogleLogin
+      clientId={GOOGLE_CLIENT_ID}
+      theme="dark"
+      buttonText="Sign In with Google"
+      className="bg-blue-600 hover:bg-blue-700"
+      onSuccess={(response) => {
+        // setLoginLoading(true);
+        loginUser(userDispatch, history, response, state);
+      }}
+      onFailure={(err) => {
+        console.log(err);
+        // setLoginLoading(false);
+      }}
+    />
+  );
+};
 
+function LandingPage() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 700,
+      easing: 'ease-out-cubic',
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto';
+    window.scroll({ top: 0 });
+    document.querySelector('html').style.scrollBehavior = '';
+    focusHandling('outline');
+  }, [pathname]);
+  return (
+    <>
       <div className="flex flex-col min-h-screen overflow-hidden">
-        <Header />
+        <Header AuthComponent={AuthComponent} />
         <main className="flex-grow">
-          <HeroHome />
+          <HeroHome AuthComponent={AuthComponent} />
         </main>
       </div>
-
     </>
   );
 }
